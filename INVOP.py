94c054a0e6e71b10f7741a2abe4def50e986d99d
Alan Contreras <47700679+AlanFcoCG@users.noreply.github.com>
2020-12-06 19:10:28 -0600

    Create INVOP.py

---
+++ INVOP.py
@@ -0,0 +1,21 @@
+filavars=[]
+
+
+
+num_x = int(input("Ingrese numero de X's: "))
+num_s = int(input("Ingrese numero de S's: "))
+
+
+
+columnas = num_x + num_s + 2
+filas = num_s + 1
+
+
+
+Matriz = [[columnas],[filas]]
+
+
+
+num_variables=int(input('Introduzca el numero de variables: '))
+for i in range(num_variables):
+    filavars.append()
