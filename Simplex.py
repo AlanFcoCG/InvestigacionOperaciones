747ad57feff1bbc404f08c505b36e8a51f640850
Miperromehabla <75592554+Miperromehabla@users.noreply.github.com>
2020-12-07 20:27:30 -0600

    Update Simplex.py
    Estaban mal unas cosas, ya saca bien la fila y columna, aparte dividi entre las funciones y el codigo principal

--- Simplex.py
+++ Simplex.py
@@ -1,5 +1,3 @@
-
-
 matrix=[[1, -180, -90, 0, 0, 0], [0, 1, 0, 1, 0, 6], [0, 0, 1, 0, 1, 4]]
 
 for i in range(len(matrix)):
@@ -15,23 +13,25 @@ def detectar_pivote(mref):
 
     return pivote
 
-
-# < >
-col_piv=detectar_pivote(matrix)
-
 def detectar_piv_fila(columna, matrix):
     radio=[]
     minimo=0
+    piv_fil=0
     for pos_fila in range(len((matrix))):
         try:
-            radio.append(float(matrix[pos_fila][int(columna)]/matrix[pos_fila][-1]))
+            radio.append(float((matrix[pos_fila][-1])/(matrix[pos_fila][int(columna)])))
         except ZeroDivisionError:
             radio.append(0)
         print(radio[pos_fila])
-        if radio[pos_fila] < minimo and (radio[pos_fila]>0 or radio[pos_fila]!=0):
-            minimo=c
+        if radio[pos_fila] > minimo and pos_fila > 0:
+            minimo=radio[pos_fila]
             piv_fil=pos_fila
-
+    print("El radio minimo es: " + str(minimo))
     return piv_fil
 
-print(detectar_piv_fila(col_piv, matrix))
+
+#--------------------MAIN-------------------------
+
+col_piv=detectar_pivote(matrix)
+fil_piv=detectar_piv_fila(col_piv, matrix)
+print("Columna privote: "+str(col_piv)+" Fila pivote: "+str(fil_piv))
